501095cf69024c225f94ad059b2e5a3128428228
Rafael <rafael2005leal@gmail.com>
2025-11-28 20:27:18 -0300

    Implement JWT authentication and update login logic
    Added JWT token generation to the login endpoint in auth.py, using a mock user for development. Updated server.py to set SECRET_KEY from environment variables. Moved login.html to templates directory and added requirements.txt for dependency management.

--- backend/src/routes/auth.py
+++ backend/src/routes/auth.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, Blueprint, request
+from flask import Flask, jsonify, Blueprint, request, current_app
 from database import get_db_connection
 from flask_bcrypt import Bcrypt
+import jwt
+import datetime
 
 auth_bp = Blueprint('auth', __name__)
 bcrypt = Bcrypt()
@@ -14,27 +16,85 @@ def login():
     if not email or not senha_digitada:
         return jsonify({"Erro": "Email e senha são obrigatórios"}), 400
     
-    conn = get_db_connection()
-    if not conn:
-        return jsonify({"Erro": "Erro ao conectar ao banco de dados"}), 500
+    ############Lógia de Mock para desenvolvimento########################
+
+    print(f"Tentando logar com {email} e {senha_digitada}")
+
+    if email == "rafael.leal@neodent.com":
+        hash_falso = bcrypt.generate_password_hash("Supergelo1!").decode('utf-8')
+
+        usuario_encontrado = (999, hash_falso)
+    else:
+        usuario_encontrado = None
     
     try:
-        cursor = conn.cursor()
-
-        query = "SELECT Email, Senha from apt_usuario WHERE Email = ? AND IsAtivo = 1"
-        cursor.execute(query, (email,))
-
-        usuario_encontrado = cursor.fetchone()
-
         if usuario_encontrado:
+            id_usuario = usuario_encontrado[0]
             senha_hash_banco = usuario_encontrado[1]
 
             if bcrypt.check_password_hash(senha_hash_banco, senha_digitada):
-                return jsonify({"Mensagem": "Login com sucesso!", "user": usuario_encontrado[0]}), 200         
+
+                payload = {
+                    'user_id': id_usuario,
+                    'email': email,
+                    'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=12)
+                }
+
+                secret_key = current_app.config.get('SECRET_KEY')
+
+                token = jwt.encode(payload, secret_key, algorithm="HS256")
+
+                return jsonify({
+                    "Mensagem": "Login com sucesso!",
+                    "token": token, 
+                    "user_id": id_usuario
+                    }), 200
+        
         return jsonify({"Mensagem": "Email ou senha incorretos"}), 401
     
     except Exception as e:
         return jsonify({"erro": f"Erro interno {str(e)}"}), 500
     
-    finally:
-        conn.close()
+
+    #######################################################################
+
+    # conn = get_db_connection()
+    # if not conn:
+    #     return jsonify({"Erro": "Erro ao conectar ao banco de dados"}), 500
+    
+    # try:
+    #     cursor = conn.cursor()
+
+    #     query = "SELECT ID, Senha from apt_usuario WHERE Email = ? AND IsAtivo = 1"
+    #     cursor.execute(query, (email,))
+
+    #     usuario_encontrado = cursor.fetchone()
+
+    #     if usuario_encontrado:
+    #         id_usuario = usuario_encontrado[0]
+    #         senha_hash_banco = usuario_encontrado[1]
+
+    #         if bcrypt.check_password_hash(senha_hash_banco, senha_digitada):
+
+    #             payload = {
+    #                 'user_id': id_usuario,
+    #                 'email': email,
+    #                 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=12)
+    #             }
+
+    #             secret_key = current_app.config.get('SECRET_KEY')
+    #             token = jwt.encode(payload, secret_key, algorithm="HS256")
+
+    #             return jsonify({
+    #                 "Mensagem": "Login com sucesso!",
+    #                 "token": token, 
+    #                 "user_id": id_usuario
+    #                 }), 200
+
+    #     return jsonify({"Mensagem": "Email ou senha incorretos"}), 401
+    
+    # except Exception as e:
+    #     return jsonify({"erro": f"Erro interno {str(e)}"}), 500
+    
+    # finally:
+    #     conn.close()
--- backend/src/server.py
+++ backend/src/server.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+import os
 from routes.auth import auth_bp
 
 app = Flask(__name__)
 load_dotenv()
 
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'minha_chave_secreta')
 app.register_blueprint(auth_bp, url_prefix='/auth')
 
 @app.route("/")
