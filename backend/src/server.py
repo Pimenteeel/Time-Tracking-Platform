4eb923f2c3d0a3f9c89428cffd7b90ce8a952882
Rafael <rafael2005leal@gmail.com>
2025-11-29 00:21:52 -0300

    Add login page template and update Flask app config
    Configured Flask to use custom template and static directories, added a new login page route rendering a new login.html template, and moved the login.js file to the static directory. Updated the home route message to direct users to the new login page.

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,17 +1,28 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from dotenv import load_dotenv
 import os
-from routes.auth import auth_bp
+from src.routes.auth import auth_bp
 
-app = Flask(__name__)
 load_dotenv()
 
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+template_dir = os.path.join(base_dir, 'src', 'templates')
+static_dir = os.path.join(base_dir, 'src', 'static')
+
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'minha_chave_secreta')
+
 app.register_blueprint(auth_bp, url_prefix='/auth')
+
+@app.route("/login-page")
+def login_page():
+    return render_template('login.html')
 
 @app.route("/")
 def home():
-    return "<h1>Rota: /auth/login<h1>"
+    return "<h1>Vá para o login: /login-page<h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
